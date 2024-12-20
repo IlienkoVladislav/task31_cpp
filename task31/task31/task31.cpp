d21d6f4012f39c484e8683ea2955feec65315ab9
IlienkoVladislav <147603955+IlienkoVladislav@users.noreply.github.com>
2024-12-20 11:40:02 +0200

    Add files via upload

---
+++ task31/task31/task31.cpp
@@ -0,0 +1,63 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <tchar.h>
+
+void ПомилкаВиходу(const std::string& повідомлення) {
+    std::cerr << повідомлення << " (error code: " << GetLastError() << ")" << std::endl;
+    exit(EXIT_FAILURE);
+}
+
+int main() {
+    HANDLE ВхіднийФайл = CreateFile(
+        TEXT("D:\\Univer\\cpp\\task31\\input.txt"), 
+        GENERIC_READ,
+        0,
+        NULL,
+        OPEN_EXISTING,
+        FILE_ATTRIBUTE_NORMAL,
+        NULL
+    );
+
+    if (ВхіднийФайл == INVALID_HANDLE_VALUE) {
+        ПомилкаВиходу("Cant to open");
+    }
+
+    HANDLE ВихіднийФайл = CreateFile(
+        TEXT("output.txt"),
+        GENERIC_WRITE,
+        0,
+        NULL,
+        CREATE_ALWAYS,
+        FILE_ATTRIBUTE_NORMAL,
+        NULL
+    );
+
+    if (ВихіднийФайл == INVALID_HANDLE_VALUE) {
+        CloseHandle(ВхіднийФайл);
+        ПомилкаВиходу("cannot create file");
+    }
+
+    const DWORD РозмірБуфера = 4096;
+    char Буфер[РозмірБуфера];
+    DWORD Прочитано = 0, Записано = 0;
+
+    while (ReadFile(ВхіднийФайл, Буфер, РозмірБуфера, &Прочитано, NULL) && Прочитано > 0) {
+        if (!WriteFile(ВихіднийФайл, Буфер, Прочитано, &Записано, NULL)) {
+            CloseHandle(ВхіднийФайл);
+            CloseHandle(ВихіднийФайл);
+            ПомилкаВиходу("failed is read");
+        }
+    }
+
+    if (GetLastError() != ERROR_SUCCESS && GetLastError() != ERROR_HANDLE_EOF) {
+        CloseHandle(ВхіднийФайл);
+        CloseHandle(ВихіднийФайл);
+        ПомилкаВиходу("failed is input read");
+    }
+
+    CloseHandle(ВхіднийФайл);
+    CloseHandle(ВихіднийФайл);
+
+    std::cout << "file is created" << std::endl;
+    return 0;
+}
